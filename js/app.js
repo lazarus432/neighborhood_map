97e4160596ac0906b37906f5ce9f5c00c72121d1
Duex <deux02030@gmail.com>
2017-09-16 14:35:43 -0400

    add 2 more attractions and update populate info window function

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ var attractionsList = [];
 // populate attractions array with name and lat longs 
 var attractions = [
 {
-  name: "Golden Gate Park",
-  location: {lat: 37.76904, lng: -122.483519}
+  name: "Golden Gate Bridge",
+  location: {lat: 37.8197222, lng: -122.4788889}
 },
 {
   name: "Fisherman's Wharf",
@@ -33,6 +33,14 @@ var attractions = [
 {
   name: "AT&T Park",
   location: {lat: 37.778644, lng: -122.38938}
+},
+{
+  name: "San Fransisco Zoo",
+  location: {lat: 37.732956, lng: -122.502953}
+},
+{
+  name: "California Academy of Sciences",
+  location: {lat: 37.7699, lng: -122.4661}
 }
 ];
 
@@ -85,7 +93,7 @@ function initMap() {
     }
 ]
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.733795, lng: -122.446747},
+    center: {lat: 37.8102062, lng: -122.4180268},
     styles: styles,
     mapTypeControl: false,
     zoom: 12
@@ -145,18 +153,6 @@ var icon_highlighted = markerIcon('FFFF33');
   document.getElementById('hide-attraction').addEventListener('click', hideAttractions);
 
 
-// takes in a color and creates a new marker icon of that color.
-function markerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21, 34));
-  return markerImage;
-}
-
-
 // populate info window with attraction name
 function populateInfoWindow(marker, infowindow) {
   if (infowindow.marker != marker) {
@@ -167,33 +163,35 @@ function populateInfoWindow(marker, infowindow) {
     infowindow.addListener('closeclick', function() {
       infowindow.marker = null;
     });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
+      var streetViewService = new google.maps.StreetViewService();
+      var radius = 50;
 
-    function streetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-        infowindow.setContent('<div>' + marker.name + '</div><div id = "pano"></div>');
-        var panoramaOptions = {
-          position: nearStreetViewLocation,
-          pov: {
-            heading: heading,
-            pitch: 30
-          }
-        };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-        infowindow.setContent('<div>' + marker.name + '</div>' +
-          '<div>No Street View Found</div>');
+      function getStreetView(data, status) {
+        if (status == google.maps.StreetViewStatus.OK) {
+          var nearStreetViewLocation = data.location.latLng;
+          var heading = google.maps.geometry.spherical.computeHeading(
+            nearStreetViewLocation, marker.position);
+            infowindow.setContent('<div>' + marker.name + '</div><div id="pano"></div>');
+            var panoramaOptions = {
+              position: nearStreetViewLocation,
+              pov: {
+                heading: 150,
+                pitch: 20
+              }
+            };
+          var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), panoramaOptions);
+        } else {
+          infowindow.setContent('<div>' + marker.name + '</div>' +
+            '<div>No Street View Found</div>');
+        }
       }
+      // use streetview service to get the closest streetview image within
+      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+      // open the infowindow on the correct marker.
+      infowindow.open(map, marker);
     }
-    streetViewService.getPanoramaByLocation(marker.position, radius, streetView);
-    infowindow.open(map, marker);
   }
-}
 }
 
 // show attractions function
@@ -210,4 +208,15 @@ function hideAttractions() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
+}
+
+// takes in a color and creates a new marker icon of that color.
+function markerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21, 34));
+  return markerImage;
 }
