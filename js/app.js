8a56c5dea9f111714d103c4a0425aecac9f4a030
Duex <deux02030@gmail.com>
2017-09-17 15:35:56 -0400

    minor fixes

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@ var attractions = [
   location: {lat: 37.8102062, lng: -122.4180268}
 },
 {
+  name: "Pier 39",
+  location: {lat: 37.8096506, lng: -122.410249}
+},
+{
   name: "Alcatraz Island",
   location: {lat: 37.826039, lng: -122.4219159}
 },
@@ -32,12 +36,36 @@ var attractions = [
   location: {lat: 37.801663, lng: -122.447909}
 },
 {
+  name: "Lombard Street",
+  location: {lat: 37.800129, lng: -122.434215}
+},
+{
+  name: "DeYoung Museum",
+  location: {lat: 37.7713188, lng: -122.4691383}
+},
+{
+  name: "Exploratorium",
+  location: {lat: 37.8008, lng: -122.3986}
+},
+{
+  name: "SFMOMA",
+  location: {lat: 37.7857, lng: -122.4011}
+},
+{
+  name: "Angel Island State Park",
+  location: {lat: 37.8636, lng: -122.4319}
+},
+{
   name: "AT&T Park",
   location: {lat: 37.778644, lng: -122.38938}
 },
 {
   name: "San Fransisco Zoo",
   location: {lat: 37.732956, lng: -122.502953}
+},
+{
+  name: "Golden Gate Park",
+  location: {lat: 37.76904, lng: -122.483519}
 },
 {
   name: "California Academy of Sciences",
@@ -185,13 +213,15 @@ self.filterList = ko.computed(function() {
   return ko.utils.arrayFilter(self.markerList(), function(list) {
     var result = (list.name.toLowerCase().search(go) >= 0);
     list.marker.setVisible(result);
-    list.marker.addListener('click', function(){
+    list.marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfoWindow);
       list.marker.setAnimation(google.maps.Animation.BOUNCE);
       setTimeout(function(){list.marker.setAnimation(null); }, 1500);
     });
     return result;
   });
 });
+
 
 // populate info window with attraction information
 function populateInfoWindow(marker, infowindow) {
