86233aa9aaa8c2bed6f2b7bff84cb03c29cc5288
Duex <deux02030@gmail.com>
2017-09-20 01:30:24 -0400

    style: update media screen 700-1200 px
    Style application to be responsive on a device between a min of 700px and a max of 1200px.

--- js/weather-api.js
+++ js/weather-api.js
@@ -7,7 +7,7 @@
 		type: "GET",
 		url: 'http://api.openweathermap.org/data/2.5/weather?lat=37.733795&lon=-122.446747&APPID=49f75f46bfbc60e9a7edf7760cc11716&units=imperial',
 		success: function(weather) {
-			$weather.append('<li>' + weather.main.temp + ' degrees</li>');
+			$weather.append('<li><h3>Current Temperature: </h3>' + weather.main.temp + ' degrees</li>');
 			$weather.append("<li><h3>High: </h3>" + weather.main.temp_max + ' degrees</li>');
 			$weather.append("<li><h3>Low: </h3>" + weather.main.temp_min + ' degrees</li>');
 		},
