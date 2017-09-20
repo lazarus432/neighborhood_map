7371cedfbe87d940032dea86866b1705358a4403
Duex <deux02030@gmail.com>
2017-09-19 23:56:25 -0400

    style: formatting; no code change

--- js/weather-api.js
+++ js/weather-api.js
@@ -7,9 +7,9 @@
 		type: "GET",
 		url: 'http://api.openweathermap.org/data/2.5/weather?lat=37.733795&lon=-122.446747&APPID=49f75f46bfbc60e9a7edf7760cc11716&units=imperial',
 		success: function(weather) {
-			$weather.append('<li>Current Temperature: ' + weather.main.temp + ' degrees</li>');
-			$weather.append("<li>Today's High: " + weather.main.temp_max + ' degrees</li>');
-			$weather.append("<li>Today's Low: " + weather.main.temp_min + ' degrees</li>');
+			$weather.append('<li>' + weather.main.temp + ' degrees</li>');
+			$weather.append("<li><h3>High: </h3>" + weather.main.temp_max + ' degrees</li>');
+			$weather.append("<li><h3>Low: </h3>" + weather.main.temp_min + ' degrees</li>');
 		},
 		error: function() {
 			alert("Coundn't retrieve current weather.")
