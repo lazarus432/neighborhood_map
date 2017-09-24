ae010576935faa9d897fbcd73c8db65f13dba70c
Duex <deux02030@gmail.com>
2017-09-24 00:26:49 -0400

    feat: added place ID
    Updated attractions array with a Place ID in order to get specific information on each attraction.

--- data/attractions.js
+++ data/attractions.js
@@ -2,62 +2,77 @@
 var attractions = [
 {
   name: "Golden Gate Bridge",
+  placeID: "49d01698f964a520fd5a1fe3",
   location: {lat: 37.8197222, lng: -122.4788889}
 },
 {
   name: "Fisherman's Wharf",
+  placeID: "4c279d763703d13a5a4aa736",
   location: {lat: 37.8102062, lng: -122.4180268}
 },
 {
   name: "Pier 39",
+  placeID: "409d7480f964a520f2f21ee3",
   location: {lat: 37.8096506, lng: -122.410249}
 },
 {
   name: "Alcatraz Island",
+  placeID: "4c7c6e777a856dcb395fe2a7",
   location: {lat: 37.826039, lng: -122.4219159}
 },
 {
   name: "Transamerica Pyramid",
+  placeID: "4a7304dcf964a5206cdb1fe3",
   location: {lat: 37.794651, lng: -122.4030265}
 },
 {
   name: "Palace of Fine Arts",
+  placeID: "4468f484f964a5204f331fe3",
   location: {lat: 37.801663, lng: -122.447909}
 },
 {
   name: "Lombard Street",
+  placeID: "49f62829f964a520136c1fe3",
   location: {lat: 37.800129, lng: -122.434215}
 },
 {
   name: "DeYoung Museum",
+  placeID: "437e6b00f964a520b82a1fe3",
   location: {lat: 37.7713188, lng: -122.4691383}
 },
 {
   name: "Exploratorium",
+  placeID: "4585a93ef964a520ac3f1fe3",
   location: {lat: 37.8008, lng: -122.3986}
 },
 {
   name: "SFMOMA",
+  placeID: "4c2256759a67a5935754dc87",
   location: {lat: 37.7857, lng: -122.4011}
 },
 {
   name: "Angel Island State Park",
+  placeID: "4a6b7ec3f964a520c7ce1fe3",
   location: {lat: 37.8636, lng: -122.4319}
 },
 {
   name: "AT&T Park",
+  placeID: "4bd2177d046076b055357371",
   location: {lat: 37.778644, lng: -122.38938}
 },
 {
   name: "San Francisco Zoo",
+  placeID: "49ca9423f964a520c0581fe3",
   location: {lat: 37.732956, lng: -122.502953}
 },
 {
   name: "Golden Gate Park",
+  placeID: "445e36bff964a520fb321fe3",
   location: {lat: 37.76904, lng: -122.483519}
 },
 {
   name: "California Academy of Sciences",
+  placeID: "",
   location: {lat: 37.7699, lng: -122.4661}
 }
 ];
